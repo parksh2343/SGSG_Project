447f736e4945739c4c22e52f79520193e71b1178
LimYunGyoo <koyahoo@naver.com>
2015-07-18 17:30:26 +0900

    join 화면에 기본 텍스트 추가

--- app/src/main/java/com/popfun/sgsg/sgsg_project/JoinActivity.java
+++ app/src/main/java/com/popfun/sgsg/sgsg_project/JoinActivity.java
@@ -1,7 +1,11 @@
 package com.popfun.sgsg.sgsg_project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
 
 /**
  * Created by LIM_YUNGYOO on 2015-07-04.
@@ -10,5 +14,14 @@ public class JoinActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join);
+
+        BootstrapButton loginBtn = (BootstrapButton) findViewById(R.id.btn_join_join);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/popfun/sgsg/sgsg_project/LoginActivity.java
+++ app/src/main/java/com/popfun/sgsg/sgsg_project/LoginActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
 
@@ -16,10 +15,7 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        BootstrapButton loginBtn = (BootstrapButton)findViewById(R.id.btn_login);
-
-//        Button loginBtn = (Button)findViewById(R.id.btn_login);
-//        Button testBtn = (Button)findViewById(R.id.testBtn);
+        BootstrapButton loginBtn = (BootstrapButton) findViewById(R.id.btn_login_login);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -28,30 +24,4 @@ public class LoginActivity extends Activity {
             }
         });
     }
-
-
-
-
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
